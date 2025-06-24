9755433603bc56b86a8ff0cab26ad5c6382ab792
ChiefNing <769472984@qq.com>
2025-06-24 11:53:24 +0800

    更新部分逻辑

--- permission-service/src/main/java/com/cheifning/permissionservice/mapper/RoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cheifning.permissionservice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cheifning.permissionservice.entity.Role;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RoleMapper extends BaseMapper<Role> {
-}
--- permission-service/src/main/java/com/cheifning/permissionservice/mapper/UserRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cheifning.permissionservice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cheifning.permissionservice.entity.UserRole;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserRoleMapper extends BaseMapper<UserRole> {
-}
